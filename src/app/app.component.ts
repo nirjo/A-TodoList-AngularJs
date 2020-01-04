28882b585a179546d34c48962050afa5390620a3
Nirmalraj <nirmalraj.joseph@gmail.com>
2020-01-04 14:59:42 +0530

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+import { Todo } from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-angular';
+
+  todoValue: string;
+  list: Todo[];
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnInit() {
+     this.list = [];
+     this.todoValue = '' ;
+
+}
+
+addItem() {
+  if (this.todoValue !== '' ) {
+    const newItem: Todo = {
+      id: Date.now(),
+      value: this.todoValue,
+      isDone: false
+    };
+    this.list.push(newItem);
+  }
+  this.todoValue = '';
+}
+
+deleteItem(id: number) {
+  this.list = this.list.filter(item => item.id !== id);
+
+}
 }
